954f3579774b186a0c9764abc2ca0109096f38d9
Taulan9 <taulan_47@mail.ru>
2021-02-17 14:13:17 +0300

    Нахождение расстояний между городами

--- 00_distance.py
+++ 00_distance.py
@@ -9,17 +9,30 @@ sites = {
     'Paris': (480, 480),
 }
 
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
 
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** .5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
+
+london_moscow = ((london[0] - moscow[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
+
+paris_london = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
+paris_moscow = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
 
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 distances = {}
 
-# TODO здесь заполнение словаря
+distances['Moscow'] = {}
+distances['Moscow']['London'] = [moscow_london]
+distances['Moscow']['Paris'] = [moscow_paris]
+
+distances['London'] = {}
+distances['London']['Moscow'] = [moscow_london]
+distances['London']['Paris'] = [moscow_paris]
 
 print(distances)
-
-
-
-
