e6b714f6ffbea588c98d398e4ea03af38dc50272
Taulan9 <taulan_47@mail.ru>
2021-03-07 18:30:18 +0300

    Исправление кода

--- 00_distance.py
+++ 00_distance.py
@@ -9,6 +9,9 @@ sites = {
     'Paris': (480, 480),
 }
 
+# Составим словарь словарей расстояний между ними
+# расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
+
 moscow = sites['Moscow']
 london = sites['London']
 paris = sites['Paris']
@@ -22,9 +25,6 @@ london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
 paris_london = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
 paris_moscow = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
 
-# Составим словарь словарей расстояний между ними
-# расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
-
 distances = {}
 
 distances['Moscow'] = {}
@@ -33,6 +33,6 @@ distances['Moscow']['Paris'] = [moscow_paris]
 
 distances['London'] = {}
 distances['London']['Moscow'] = [moscow_london]
-distances['London']['Paris'] = [moscow_paris]
+distances['London']['Paris'] = [london_paris]
 
 print(distances)
